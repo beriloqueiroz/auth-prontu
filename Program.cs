92067f7c1aabd60c2857869bfba5136719790d34
berilo queiroz <luiz.berilo@buson.com.br>
2023-11-02 17:58:53 -0300

    ajustando url

--- Program.cs
+++ Program.cs
@@ -12,13 +12,13 @@ builder.Services.InjectMySwagger();
 var app = builder.Build();
 
 Console.WriteLine("Setting environment variables for each target..., how production\n ");
-Environment.SetEnvironmentVariable("UrlBase", "https://localhost:5100");
+Environment.SetEnvironmentVariable("UrlBase", "https://localhost:5000");
 
 if (app.Environment.IsDevelopment())
 {
     app.UseMyDocumentation();
     Console.WriteLine("Setting environment variables for each target..., how development\n ");
-    Environment.SetEnvironmentVariable("UrlBase", "http://localhost:5100");
+    Environment.SetEnvironmentVariable("UrlBase", "http://localhost:5000");
 }
 
 app.UseHttpsRedirection();
